8296bdc9f0463cfc6ebd137396971c7b2737e5d0
phpilatso <phenyopilatso@momentum.co.za>
2020-09-23 17:15:26 +0200

    Initial Commit

---
+++ src/main/java/com/example/adminmanager/AdminmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.adminmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/adminmanager/AdminmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adminmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
